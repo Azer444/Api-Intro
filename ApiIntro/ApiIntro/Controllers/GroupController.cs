f8833b59f1ff81e2944ba2abe0afee3083cdedac
Azer444 <azerrh@code.edu.az>
2022-11-16 03:49:47 +0400

    Api-Intro

---
+++ ApiIntro/ApiIntro/Controllers/GroupController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<Group> groups = new List<Group>();
+            Group group1 = new()
+            {
+                Id= 1,
+                Name = "P130",
+                Capacity= 16
+            };
+            Group group2 = new()
+            {
+                Id = 1,
+                Name = "EL214",
+                Capacity = 18
+            };
+            Group group3 = new()
+            {
+                Id = 1,
+                Name = "P126",
+                Capacity = 22
+            };
+
+            groups.Add(group1);
+            groups.Add(group2);
+            groups.Add(group3);
+
+            return Ok(groups);
+
+        }
+    }
+}
---
+++ ApiIntro/ApiIntro/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIntro.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            string name = "Azer" + id;
+            return Ok(name);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<string> names = new List<string>()
+            {
+                "Azer","Maqa","Huseyn"
+            };
+            return Ok(names);
+        }
+    }
+}
